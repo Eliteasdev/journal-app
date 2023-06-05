61938d78eb7d0b705a16d9bb1220eb310988797d
Eliteasdev <elitefirelight.dev@gmail.com>
2023-06-05 14:00:25 -0600

    AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,17 +1,12 @@
 import {Link as RouterLink} from "react-router-dom";
 import Google from "@mui/icons-material/Google"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLaout } from "../layout/AuthLaout";
 
 export const LoginPage = () => {
   return (
-    <Grid container spacing={0} direction="column" alignItems="center" justifyContent="center" sx={{minHeight:"100vh", backgroundColor:"primary.main", padding:4}}>
-      
-      <Grid item className="box-shadow"
-      xs={3}
-      sx={{backgroundColor:"white", padding:3, borderRadius:2}}
-      >
-        <Typography variant="h5" sx={{mb:1}}>Login</Typography>
-        <form action="">
+    <AuthLaout title="Login">
+      <form action="">
           <Grid container>
 
             <Grid item xs={12} sx={{mt:2}}>
@@ -47,7 +42,6 @@ export const LoginPage = () => {
 
           </Grid>
         </form>
-      </Grid>
-    </Grid>
+    </AuthLaout>
   )
 }
