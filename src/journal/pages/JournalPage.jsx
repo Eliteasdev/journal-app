1beead40dee1216f6581bca845bcc256d8948ac8
Eliteasdev <elitefirelight.dev@gmail.com>
2023-06-05 13:16:10 -0600

    Creación de la Loginpage
    Creación de los inputs para la LoginPage

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,11 @@
+import MailOutline  from "@mui/icons-material/MailOutline"
+import { Typography } from "@mui/material"
+
+export const JournalPage = () => {
+  return (
+    <>
+    <Typography variant="h1">JournalPage</Typography>
+    <MailOutline />
+    </>
+  )
+}
