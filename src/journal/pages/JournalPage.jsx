0e572066a79030397c6c9d49f09effae38866adc
Eliteasdev <elitefirelight.dev@gmail.com>
2023-06-09 13:37:41 -0600

    NothingSelectedView
    View que se muestra cuando no hay una nota seleccionada.

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../Layout/JournalLayout"
+import { NothingSelectedView } from "../views/NothingSelectedView"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, facilis?</Typography>
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, facilis?</Typography> */}
+      <NothingSelectedView/>
     </JournalLayout>
   )
 }
