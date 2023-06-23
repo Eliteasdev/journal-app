b14a4283adb801dd47f7a86f8a9974e432afe5cb
Eliteasdev <elitefirelight.dev@gmail.com>
2023-06-23 13:04:52 -0600

    Boton Flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,26 @@
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../Layout/JournalLayout"
 import { NoteView } from "../views/NoteView"
 import { NothingSelectedView } from "../views/NothingSelectedView"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       {/* <Typography>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio, facilis?</Typography> */}
-      {/* <NothingSelectedView/> */}
-      <NoteView/>
+      <NothingSelectedView/>
+      {/* <NoteView/> */}
+      <IconButton size="large"
+        sx={{color:"white",
+          backgroundColor:"error.main",
+          ":hover":{backgroundColor: "error.main", opacity: 0.9},
+          position:"fixed",
+          right:50,
+          bottom:50
+        }}
+      >
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
     </JournalLayout>
   )
 }
